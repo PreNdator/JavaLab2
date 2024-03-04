1f57bfd24cc98b9162c4e47b7b94b36b580756fc
Tim <ledenev.03@mail.ru>
2024-03-04 22:28:40 +0300

    User input

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,10 +1,21 @@
 import expression.ExpressionCalculator;
 
+import java.util.Scanner;
+
 
 public class Main {
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Enter a mathematical expression:");
+        String expression = scanner.nextLine();
+
         ExpressionCalculator calculator = new ExpressionCalculator();
 
-        System.out.println(calculator.calculate("sin(3.14) + 2 * (4 - 1)"));
+        try {
+            double result = calculator.calculate(expression);
+            System.out.println("Result of calculation: " + result);
+        } catch (IllegalArgumentException e) {
+            System.out.println("Error: " + e.getMessage());
+        }
     }
 }
